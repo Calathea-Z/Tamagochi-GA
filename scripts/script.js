027e29c9e82fac71b39a743760025ec0c2650ccf
Calathea-Z <zsykes21@gmail.com>
2022-11-10 18:48:18 -0500

    Changed a few things. Saving work for now.

--- scripts/script.js
+++ scripts/script.js
@@ -2,9 +2,16 @@
 const titlePageButton = document.querySelector("#begin");
 console.log(titlePageButton);
 
-const dogHover = document.querySelector("#boxer");
-console.log(dogHover);
+// const dogHover = document.querySelector("#boxer");
+// audio.play();
+
+
 
 titlePageButton.addEventListener('click', () => {
     window.location.href = "./pages/character_choice.html";
 });
+
+// dogHover.addEventListener('mouseenter', () => {
+//     const audio = document.getElementById("dog_panting");
+//     audio.Play();
+// });
