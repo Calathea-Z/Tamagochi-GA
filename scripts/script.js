37160ce75a442c281650b008aeecd64c59033d23
Calathea-Z <zsykes21@gmail.com>
2022-11-10 18:16:41 -0500

    Updated the majority of the CSS / HTML / JS for intro page. Linked intro page to 2nd page.

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,10 @@
-console.log("Sanity Never Came My Way")
+//Grabbing begin button on splash page.
+const titlePageButton = document.querySelector("#begin");
+console.log(titlePageButton);
+
+const dogHover = document.querySelector("#boxer");
+console.log(dogHover);
+
+titlePageButton.addEventListener('click', () => {
+    window.location.href = "./pages/character_choice.html";
+});
