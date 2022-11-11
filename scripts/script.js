3ed97e0c261d5601bc749b33345905210d0c1ad9
Calathea-Z <zsykes21@gmail.com>
2022-11-11 07:41:00 -0500

    Finished Page 1. Got audio-clip working but chrome wont allow auto play. Begin building out page 2.

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,32 @@
-//Grabbing begin button on splash page.
+                                                                //Grabbing begin button on splash page.
 const titlePageButton = document.querySelector("#begin");
 console.log(titlePageButton);
 
-// const dogHover = document.querySelector("#boxer");
-// audio.play();
+                                                                //Grabbing audio to play.
+const pantSoundHover = document.querySelector(".panting");
+console.log(pantSoundHover);
 
+                                                                //Changing button color to green on mouse enter button space.
+titlePageButton.addEventListener('mouseenter', function () {
+    this.style.backgroundColor = 'green';
+});
 
+                                                                //Changing button color to o.g. color on mouse leave button space.
+titlePageButton.addEventListener('mouseleave', function () {
+    this.style.backgroundColor = 'goldenrod';
+});
 
+                                                                //Linking to next webpage on button click.
 titlePageButton.addEventListener('click', () => {
     window.location.href = "./pages/character_choice.html";
 });
 
-// dogHover.addEventListener('mouseenter', () => {
-//     const audio = document.getElementById("dog_panting");
-//     audio.Play();
+// titlePageButton.addEventListener('mouseenter', () => {
+//     playClip();
 // });
+
+// function playClip () {
+//     let soundClip = document.querySelector(".panting");
+//     console.log(soundClip);
+//     soundClip.play();
+// }
