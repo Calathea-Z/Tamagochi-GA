98ef82e796313c30fc120dd2ced26c17ddb5c353
Calathea-Z <zsykes21@gmail.com>
2022-11-12 07:44:54 -0500

    Update page 1 with credit for sounds provider. Finish page 2. Site now stores variables for user name choice and character selection.

--- script_character.js
+++ script_character.js
@@ -1,73 +1,66 @@
-//Grabbing pupper one buttons and the div around the button.
-const selectPupperOne = document.querySelector("#boxer_button");
+//Grabbing the divs around each pupper.
 const pupperOneHover = document.querySelector(".boxer");
-//Grabbing pupper two buttons and the div around the button.
-const selectPupperTwo = document.querySelector("#bulldog_button");
 const pupperTwoHover = document.querySelector(".bulldog");
-//Grabbing pupper three buttons and the div around the button.
-const selectPupperThree = document.querySelector("#fox_terrier_button");
 const pupperThreeHover = document.querySelector(".fox_terrier");
-//Grabbing audio permission button and the audio files.
+//Grabbing audio permission button & the audio files.
 const selectAudioPermission = document.querySelector("#allow_music_button");
 const audioPantOne = new Audio(src="../sounds/dog_panting.wav");
 const audioPantTwo = new Audio(src="../sounds/squish_breathing.wav");
 const audioPantThree = new Audio(src="../sounds/happy_pant.wav");
 const audioSuccess = new Audio(src="../sounds/success.wav");
-//Grabbing the information from the name input
-let characterNameGrab = document.querySelector("#name_form");
-let characterNameValue = characterNameGrab.value;
-console.log(characterNameValue);
 //Grabbing the start button.
 const selectPlayButton = document.querySelector("#start");
-//console.log(selectPlayButton);
-//Variables for character selection and name selection
-let characterChoice = null;
+//Grabbing the user's pupper choice & name selection.
+let characterChoice = document.querySelectorAll('input[name="dog_character');
+let nameChoice = document.querySelector('#name_form')
+let selectedCharacter = " ";
+let selectedName = " ";
 
 
 //pupper one mouse-over effects
-selectPupperOne.addEventListener('mouseenter', function () {
-    pupperOneHover.style.backgroundColor = '#40ff00';
-    pupperOneHover.style.opacity = '.7';
-    pupperOneHover.style.borderRadius = '10px';
+pupperOneHover.addEventListener('mouseenter', function () {
+    this.style.backgroundColor = '#40ff00';
+    this.style.opacity = '.7';
+    this.style.borderRadius = '10px';
     audioPantOne.play();
 });
 
-selectPupperOne.addEventListener('mouseleave', function () {
-    pupperOneHover.style.backgroundColor = '';
-    pupperOneHover.style.opacity = '';
-    pupperOneHover.style.borderRadius = '';
+pupperOneHover.addEventListener('mouseleave', function () {
+    this.style.backgroundColor = '';
+    this.style.opacity = '';
+    this.style.borderRadius = '';
     audioPantOne.pause();
     audioPantOne.currentTime = 0;
 })
 
 //pupper two mouse-over effects
-selectPupperTwo.addEventListener('mouseenter', function () {
-    pupperTwoHover.style.backgroundColor = '#40ff00';
-    pupperTwoHover.style.opacity = '.7';
-    pupperTwoHover.style.borderRadius = '10px';
+pupperTwoHover.addEventListener('mouseenter', function () {
+    this.style.backgroundColor = '#40ff00';
+    this.style.opacity = '.7';
+    this.style.borderRadius = '10px';
     audioPantTwo.play();
 });
 
-selectPupperTwo.addEventListener('mouseleave', function () {
-    pupperTwoHover.style.backgroundColor = '';
-    pupperTwoHover.style.opacity = '';
-    pupperTwoHover.style.borderRadius = '';
+pupperTwoHover.addEventListener('mouseleave', function () {
+    this.style.backgroundColor = '';
+    this.style.opacity = '';
+    this.style.borderRadius = '';
     audioPantTwo.pause();
     audioPantTwo.currentTime = 0;
 })
 
 //pupper three mouse-over effects
-selectPupperThree.addEventListener('mouseenter', function () {
-    pupperThreeHover.style.backgroundColor = '#40ff00';
-    pupperThreeHover.style.opacity = '.7';
-    pupperThreeHover.style.borderRadius = '10px';
+pupperThreeHover.addEventListener('mouseenter', function () {
+    this.style.backgroundColor = '#40ff00';
+    this.style.opacity = '.7';
+    this.style.borderRadius = '10px';
     audioPantThree.play();
 });
 
-selectPupperThree.addEventListener('mouseleave', function () {
-    pupperThreeHover.style.backgroundColor = '';
-    pupperThreeHover.style.opacity = '';
-    pupperThreeHover.style.borderRadius = '';
+pupperThreeHover.addEventListener('mouseleave', function () {
+    this.style.backgroundColor = '';
+    this.style.opacity = '';
+    this.style.borderRadius = '';
     audioPantThree.pause();
     audioPantThree.currentTime = 0;
 })
@@ -95,14 +88,17 @@ selectPlayButton.addEventListener('mouseleave', function () {
 selectPlayButton.addEventListener('click', function () {
     audioSuccess.play();
     setTimeout(changePage, 1000);
+
+    selectedName = nameChoice.value;
+
+    for (characterChoice of characterChoice){
+        if(characterChoice.checked){
+            selectedCharacter = characterChoice.value;
+            break;
+        }
+    }
+
     function changePage (){
     window.location.href = "./play_game.html";
     }
 });
-// console.log(characterNameValue);
-// if (characterNameValue != ("..Travis Bark-er")){
-//     characterNameValue = characterName.value
-//     console.log("TEST - " + characterNameValue)
-// }else {
-//     console.log("FALSE");
-// }
